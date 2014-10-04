16552cf7017bd82fba25fe856ed391f563fb823d
Michael Rackers <mlr7x9@mail.missouri.edu>
2014-10-03 23:32:00 -0500

    Edited Slip Shot

--- Slip Shot/popup.js
+++ Slip Shot/popup.js
@@ -2,3 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text/html", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text/html");
+    ev.target.appendChild(document.getElementById(data));
+}
