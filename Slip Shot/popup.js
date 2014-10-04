79ee9ef83e2aa147a80f20773041e0781e646b78
Philip Cressler <philipcressler@mwc-150016.dhcp.missouri.edu>
2014-10-04 05:07:05 -0500

    added jquery library

--- Slip Shot/popup.js
+++ Slip Shot/popup.js
@@ -1,8 +1,6 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
  $('#submitMessage').submit(function (event) {
             event.preventDefault();
             $.ajax({
