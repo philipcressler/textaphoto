23fd7aa6f9d5a524cba8ed33c9b5abca658fc553
Philip Cressler <philipcressler@mwc-150016.dhcp.missouri.edu>
2014-10-04 04:18:01 -0500

    staging commit

--- Slip Shot/popup.js
+++ Slip Shot/popup.js
@@ -2,3 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+
+
+
+
+// When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+    // Cache a reference to the status display SPAN
+    statusDisplay = document.getElementById('status-display');
+    // Handle the bookmark form submit event with our addBookmark function
+    document.getElementById('submitMessage').addEventListener('submit', sendMessage);
+    // Get the event page
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        // Call the getPageInfo function in the event page, passing in 
+        // our onPageDetailsReceived function as the callback. This injects 
+        // content.js into the current tab's HTML
+        eventPage.getPageDetails(onPageDetailsReceived);
+    });
+});
