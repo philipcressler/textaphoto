31df08bfcd4d1571f93753185738c1fa69b09cf3
Philip Cressler <philipcressler@mwc-150016.dhcp.missouri.edu>
2014-10-04 05:16:06 -0500

    -

--- Slip Shot/popup.js
+++ Slip Shot/popup.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-<script language="JavaScript" type="text/javascript" src="jquery-2.1.1.min.js"></script>
 
  $('#submitMessage').submit(function (event) {
             event.preventDefault();
